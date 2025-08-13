189a617c6d4e5af1541c19a715230e765c2deab6
Arrods <Arrodsmail@gmail.com>
2025-08-13 14:02:00 +0200

    Ny fil

---
+++ Ny harddisk og skift dvd.ps1
@@ -0,0 +1,28 @@
+Flyt DVD-drevet (hvis D: allerede er taget)
+$cdrom = Get-CimInstance -ClassName Win32_Volume | Where-Object { $_.DriveLetter -eq "D:" -and $_.DriveType -eq 5 }
+if ($cdrom) {
+    Set-CimInstance -InputObject $cdrom -Property @{DriveLetter = "Z:"}
+    Write-Host "DVD-drevet flyttet til Z:"
+}
+
+#Find ny disk (Offline, ikke initialiseret)
+$newDisk = Get-Disk | Where-Object { $_.OperationalStatus -eq "Offline" -or $_.PartitionStyle -eq "RAW" } | Select-Object -First 1
+
+if (-not $newDisk) {
+    Write-Host "Ingen ny disk fundet!"
+    exit
+}
+
+#Gør disken online og initialiser
+Set-Disk -Number $newDisk.Number -IsOffline $false -IsReadOnly $false
+Initialize-Disk -Number $newDisk.Number -PartitionStyle GPT
+Write-Host "Disk initialiseret."
+
+#Opret partition og formater
+$part = New-Partition -DiskNumber $newDisk.Number -UseMaximumSize -AssignDriveLetter
+Format-Volume -Partition $part -FileSystem NTFS -NewFileSystemLabel "DataDisk" -Confirm:$false
+Write-Host "Disk formateret."
+
+#Tildel drevbogstav D:
+Set-Partition -PartitionNumber $part.PartitionNumber -DiskNumber $newDisk.Number -NewDriveLetter "D"
+Write-Host "Disk tildelt drevbogstav D:"
