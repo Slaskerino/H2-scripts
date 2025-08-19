dd7437bc16438a74e879495b8883d105a7220a68
slaskerino <asger.mathiesen@icloud.com>
2025-08-19 10:55:05 +0200

    Opdateret script

--- RLZ.ps1
+++ RLZ.ps1
@@ -1,4 +1,28 @@
-Add-DnsServerPrimaryZone `
+$zonefile = "10.0.10.in-addr.arpa.dns"
+$DomainName = "alpaco.local"
+$PrimaryDNS = "10.0.10.11" ##DC01
+$SecondaryDNS = "10.0.10.22" ##DNS02
+
+
+
+try {
+    Add-DnsServerPrimaryZone `
     -NetworkId "10.0.10.0/24" `
-    -ZoneFile "10.0.10.in-addr.arpa.dns" `
+    -ZoneFile $zonefile `
     -DynamicUpdate Secure
+    
+    Write-Host "Zonen $zonefile blev oprettet"
+}
+catch {
+    Write-Host "Zonen blev ikke oprettet grundet: $($_.Exception.Message)"
+}
+
+Add-DnsServerSecondaryZone -Name $DomainName -MasterServers $PrimaryDNS -ComputerName $SecondaryDNS -ZoneFile $zonefile
+
+try {
+    Add-DnsServerSecondaryZone -Name $DomainName -MasterServers $PrimaryDNS -ComputerName $SecondaryDNS -ZoneFile $zonefile
+    Write-Host "Serveren $SecondaryDNS blev oprettet som 2. DNS server for zonen $zonefile"
+}
+catch {
+    Write-Host "Serveren kunne ikke tilfojes grundet: $($_.Exception.Message)"
+}
