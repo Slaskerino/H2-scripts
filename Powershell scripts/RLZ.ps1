c162a6fad9b8019c9ce1f662587c9a9a9b850eb8
Arrods <Arrodsmail@gmail.com>
2025-08-20 13:29:50 +0200

    opd

--- Powershell scripts/RLZ.ps1
+++ Powershell scripts/RLZ.ps1
@@ -7,6 +7,7 @@ $SecondaryDNS = "10.0.10.22" ##DNS02
 
 
 try {
+    #Opretter en primær DNS-zone for netværket 10.0.10.0/24 med den angivne zonefil
     Add-DnsServerPrimaryZone `
     -NetworkId "10.0.10.0/24" `
     -ZoneFile $zonefile `
@@ -20,6 +21,7 @@ catch {
 }
 
 try {
+    #Her konfigureres zonetransfer til kun at sende til den specificerede sekundære DNS-server ($SecondaryDNS) og sætter den som sikker sekundær.
     Set-DnsServerPrimaryZone -Name $zone -SecureSecondaries "TransferToSecureServers" -SecondaryServers $SecondaryDNS -ComputerName $PrimaryDNS
     Write-Host "Serveren $SecondaryDNS blev oprettet som 2. DNS server for zonen $zonefile"
 }
@@ -28,6 +30,7 @@ catch {
 }
 
 try {
+    #Her konfigureres DNS-replikation, så sekundærserveren får en kopi af zonen fra primærserveren.
     Add-DnsServerSecondaryZone -Name $zone -MasterServers $PrimaryDNS -ComputerName $SecondaryDNS -ZoneFile $zonefile
     Write-Host "Serveren $SecondaryDNS blev oprettet som 2. DNS server for zonen $zonefile"
 }
