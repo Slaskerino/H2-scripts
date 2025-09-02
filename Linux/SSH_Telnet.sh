1b1ff3c838bf560d3250b0c9a11d361fba39e430
Arrods <Arrodsmail@gmail.com>
2025-09-02 09:06:53 +0200

    fguj

--- Linux/SSH_Telnet.sh
+++ Linux/SSH_Telnet.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#Opdaterer og opgraderer de pakker som er installeret på systemet.
 apt update && sudo apt upgrade
+
+#Installerer pakken SSH.
 apt install openssh-server openssh-client
 
 #Aktiver SSH genne port 22 ELLER giver SSH adgang igennem UFW igennem en subnet og port 22.
@@ -16,6 +19,8 @@ ufw allow from 10.0.0.0/8 to any port 23
 #Aktiverer ufw. OBS vær opmærksom på at det hele er opsat da, SSH adgang kan forsvinde hvis ikke opsat korrekt.
 ufw enable
 
+#Den kommando tilføjer en linje til filen /etc/inetd.conf, som fortæller inetd-tjenesten, 
+#at den skal starte en Telnet-server (/usr/sbin/telnetd) når nogen forbinder til port 23/tcp.
 echo "telnet  stream  tcp     nowait  root    /usr/sbin/tcpd  /usr/sbin/telnetd" >> /etc/inetd.conf
 sudo systemctl restart inetd
 
