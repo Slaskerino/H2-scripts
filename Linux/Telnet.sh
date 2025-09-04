ceef7ff31ddb99c1024d253dd9fb9bef516c95cc
Arrods <Arrodsmail@gmail.com>
2025-09-04 08:40:05 +0200

    changealittlebittle

--- Linux/Telnet.sh
+++ Linux/Telnet.sh
@@ -1,15 +1,3 @@
-#!/bin/bash
-#Opdaterer og opgraderer de pakker som er installeret på systemet.
-apt update && sudo apt upgrade
-
-#Installerer pakken SSH.
-apt install openssh-server openssh-client
-apt install -y ufw
-
-#Aktiver SSH gennem port 22 ELLER giver SSH adgang igennem UFW igennem en subnet og port 22.
-ufw allow ssh
-ufw allow from 10.0.0.0/8 to any port 22
-
 #Installer telnet på serveren.
 apt install telnetd -y
 
