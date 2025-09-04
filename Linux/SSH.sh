76faf0a99b7a16449195952a1b40d3d83f6f3955
Arrods <Arrodsmail@gmail.com>
2025-09-04 08:48:56 +0200

    opd

--- Linux/SSH.sh
+++ Linux/SSH.sh
@@ -7,5 +7,6 @@ apt install openssh-server openssh-client
 apt install -y ufw
 
 #Aktiver SSH gennem port 22 ELLER giver SSH adgang igennem UFW igennem en subnet og port 22.
+#Dette gøres så vi ikke mister forbindelsen når UFW aktiveres.
 ufw allow ssh
 ufw allow from 10.0.0.0/8 to any port 22
--- Linux/Telnet.sh
+++ Linux/Telnet.sh
@@ -2,10 +2,8 @@
 apt install telnetd -y
 
 #Aktiver telnet og giver den adgang igennem UFW igennem en subnet og port 23.
+#Dette gøres så vi ikke mister forbindelsen når UFW aktiveres.
 ufw allow from 10.0.0.0/8 to any port 23
-
-#Aktiverer ufw. OBS vær opmærksom på at det hele er opsat da, SSH adgang kan forsvinde hvis ikke opsat korrekt.
-ufw enable
 
 #Den kommando tilføjer en linje til filen /etc/inetd.conf, som fortæller inetd-tjenesten, 
 #at den skal starte en Telnet-server (/usr/sbin/telnetd) når nogen forbinder til port 23/tcp.
