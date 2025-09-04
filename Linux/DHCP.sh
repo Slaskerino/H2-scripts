98317421e45a3b9ef5f04e68d410f89f4ef66d3c
Arrods <Arrodsmail@gmail.com>
2025-09-04 10:28:51 +0200

    opd

--- Linux/DHCP.sh
+++ Linux/DHCP.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 #Installering af DHCP service.
+echo "Installerer DHCP servicen..."
 apt-get install isc-dhcp-server -y
 
 #Start og enable servicen dhcpp.
+echo "Starter og enabler DHCP servicen."
 systemctl start isc-dhcp-server
 systemctl enable isc-dhcp-server
 
-#Konfigurer selve DHCP servicen.
-# Filen vi vil ændre
+#Her konfigurer vi selve DHCP servicen.
+echo "Den rigtige interface vælges til servicen."
+#Vi vil ændre selve Filen.
 FILE="/etc/default/isc-dhcp-server"
 # Brug sed til at ændre INTERFACESv4=""
 sed -i 's/^INTERFACESv4=".*"/INTERFACESv4="enp6s19"/' "$FILE"
 
 
 #Nederst laver vi en tee for at indtaste alt nedenstående data på samme tid inkl. linjeskift i filen dhcp.conf.
+echo "DHCP config filen bliver configureret."
 tee -a /etc/dhcp/dhcpd.conf > /dev/null <<EOT
 subnet 192.168.2.0 netmask 255.255.255.0 {
 range 192.168.2.50 192.168.2.100;
@@ -25,4 +29,6 @@ option routers 192.168.2.1;
 EOT
 
 #Genstarter servicen for DHCP.
+echo "DHCP servicen genstarter"
 systemctl restart isc-dhcp-server
+echo "DHCP servicen er nu oppe og køre. God fornøjelse!"
