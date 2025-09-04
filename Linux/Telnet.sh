4217fbe11dd865b488834c5969fb606333a2b4d6
Arrods <Arrodsmail@gmail.com>
2025-09-04 09:24:28 +0200

    opd

--- Linux/Telnet.sh
+++ Linux/Telnet.sh
@@ -3,7 +3,7 @@ apt install telnetd -y
 
 #Aktiver telnet og giver den adgang igennem UFW igennem en subnet og port 23.
 #Dette gøres så vi ikke mister forbindelsen når UFW aktiveres.
-ufw allow from 10.0.0.0/8 to any port 23
+ufw allow from 10.0.0.0/16 to any port 23
 
 #Den kommando tilføjer en linje til filen /etc/inetd.conf, som fortæller inetd-tjenesten, 
 #at den skal starte en Telnet-server (/usr/sbin/telnetd) når nogen forbinder til port 23/tcp.
