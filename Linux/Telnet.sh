8ba4cea4e0093a174344e38724a45f0a9fbb29d9
slaskerino <asger.mathiesen@icloud.com>
2025-09-08 13:05:17 +0200

    asdf

--- Linux/Telnet.sh
+++ Linux/Telnet.sh
@@ -1,14 +1,16 @@
 #Installer telnet på serveren.
 apt install telnetd -y
 
-#Aktiver telnet og giver den adgang igennem UFW igennem en subnet og port 23.
-#Dette gøres så vi ikke mister forbindelsen når UFW aktiveres.
-ufw allow from 10.0.0.0/16 to any port 23
+#Giver telnet adgang igennem IPtables fra subnet 10.0.0.0/16 på port 23.
+
+sudo iptables -A INPUT -p tcp --dport 23 -s 10.0.0.0/16 -j ACCEPT
 
 #Den kommando tilføjer en linje til filen /etc/inetd.conf, som fortæller inetd-tjenesten, 
 #at den skal starte en Telnet-server (/usr/sbin/telnetd) når nogen forbinder til port 23/tcp.
 echo "telnet  stream  tcp     nowait  root    /usr/sbin/tcpd  /usr/sbin/telnetd" >> /etc/inetd.conf
 sudo systemctl restart inetd
+
+
 
 #######OBS nedenstående sletter din telnet forbindelse igen.#######
 apt autoremove telnetd --purge
