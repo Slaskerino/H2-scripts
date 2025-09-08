196432711a269b3d56bfd3028bb4ec73bb956bf5
slaskerino <asger.mathiesen@icloud.com>
2025-09-08 13:01:54 +0200

    asd

--- Linux/SSH.sh
+++ Linux/SSH.sh
@@ -7,5 +7,6 @@ apt install openssh-server openssh-client
 
 #Aktiver SSH gennem port 22 ELLER giver SSH adgang igennem firewall igennem en subnet og port 22.
 #Dette gøres så vi ikke mister forbindelsen når UFW aktiveres.
-ufw allow ssh
-ufw allow from 10.0.0.0/16 to any port 22
+
+
+sudo iptables -A INPUT -p tcp --dport 22 -s 10.0.0.0/16 -j ACCEPT
