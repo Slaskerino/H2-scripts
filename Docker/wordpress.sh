83b7511a7f1def0507695854ec01367ec7774f89
Arrods <Arrodsmail@gmail.com>
2025-09-15 13:13:16 +0200

    wordpress

---
+++ Docker/wordpress.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Tjek om Docker kører
+docker --version
+docker compose version
+
+#Hvis ikke, installer med
+apt update && apt install -y docker.io docker-compose
+systemctl enable --now docker
+
+#Lav en projektmappe ved navn wordpress
+mkdir ~/wordpress && cd ~/wordpress
+
+#Opretter en docker-compose.yml fil
+version: '3.9'
+
+services:
+  db:
+    image: mysql:5.7
+    volumes:
+      - db_data:/var/lib/mysql
+    restart: always
+    environment:
+      MYSQL_ROOT_PASSWORD: rootpass
+      MYSQL_DATABASE: wordpress
+      MYSQL_USER: wpuser
+      MYSQL_PASSWORD: wppass
+
+  wordpress:
+    depends_on:
+      - db
+    image: wordpress:latest
+    ports:
+      - "8080:80"
+    restart: always
+    environment:
+      WORDPRESS_DB_HOST: db:3306
+      WORDPRESS_DB_USER: wpuser
+      WORDPRESS_DB_PASSWORD: wppass
+      WORDPRESS_DB_NAME: wordpress
+    volumes:
+      - wp_data:/var/www/html
+
+volumes:
+  db_data: {}
+  wp_data: {}
+
+#Starter Wordpress
+docker compose up -d
+
