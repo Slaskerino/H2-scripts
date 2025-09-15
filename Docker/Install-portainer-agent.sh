bb33c9ebcc70de3367ebcf31cfb690994fe36a80
slaskerino <asger.mathiesen@icloud.com>
2025-09-15 12:28:26 +0200

    asd

---
+++ Docker/Install-portainer-agent.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# installere portainer agenten som en docker container på hosten.
+docker run -d \
+  -p 9001:9001 \
+  --name portainer_agent \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  -v /:/host \
+  portainer/agent:2.33.1
